b658a1957cb8209f8f80fb5b12ef9a37b1f77db1
Dominic Go <dominicstop@gmail.com>
2019-09-16 15:48:44 +0800

    added ios nativeModules initial boilerplate

---
+++ ios/Native Modules/nativeModules.m
@@ -0,0 +1,10 @@
+//
+//  nativeModules.m
+//  AO3Client
+//
+//  Created by Dominic Go on 16/09/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTBridgeModule.h"
---
+++ ios/Native Modules/nativeModules.swift
@@ -0,0 +1,9 @@
+//
+//  nativeModules.swift
+//  AO3Client
+//
+//  Created by Dominic Go on 16/09/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
